4c03afcfd7237b6af951a4d6a42e76a28f69c0d3
rituraj97 <41967963+rituraj97@users.noreply.github.com>
2020-02-23 23:02:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+
+
+TweenMax.to(".loading-screen", 4, {
+    delay: 1.2,
+    top: "-110%",
+    ease: Expo.easeInOut
+});
+
+
+TweenMax.from(".main", 3, {
+    delay: 3.5,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".menu", 2, {
+    delay: 4.3,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".pulogo", 3, {
+    delay: 3.5,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+});
+
+
+
